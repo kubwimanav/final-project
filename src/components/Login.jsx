abb05d0d0052f5a0e062178b795fb9fdcf74b643
kubwimanav <kubwimanavincent2020@gmail.com>
2025-05-02 22:56:54 +0200

    login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -23,51 +23,53 @@ function Login() {
   };
 
   return (
-    <div className="Login-container">
-      <div className="Login-brand-container">
-        <div className="brand-logo">
-          <div className="soccer-ball-icon"></div>
-          <h4>Digital Lost and Found System</h4>
+    <div className="login-page-wrapper">
+      <div className="Login-container">
+        <div className="Login-brand-container">
+          <div className="brand-logo">
+            <div className="soccer-ball-icon"></div>
+            <h3>Digital Lost and Found System</h3>
+          </div>
+          <p>Don't have an account?</p>
+          <Link to="/signup" className="signin-link">
+            <button className="signin-button">Sign Up</button>
+          </Link>
         </div>
-        <p>Don't have an account?</p>
-        <Link to="/signup" className="signin-link">
-          <button className="signin-button">Sign in</button>
-        </Link>
-      </div>
-      <div className="login-form-container">
-        <h2> Welcome Back! </h2>
-        <p className="signup-subtitle">Please login to continue</p>
+        <div className="signup-form-container">
+          <h1>Welcome Back!</h1>
+          <p className="signup-subtitle">Please login to continue</p>
 
-        <form onSubmit={handleSubmit} className="Login-form">
-          <div className="form-group">
-            <input
-              type="email"
-              id="email"
-              name="email"
-              placeholder="Email Address"
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <input
-              type="password"
-              id="password"
-              name="password"
-              placeholder="Password"
-              value={formData.password}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div className="forgot-password">
-            <Link to="/forgot-password">Forgot password?</Link>
-          </div>
-          <button type="submit" className="signup-button">
-            Login
-          </button>
-        </form>
+          <form onSubmit={handleSubmit} className="Login-form">
+            <div className="form-group">
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Email Address"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Password"
+                value={formData.password}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="forgot-password">
+              <Link to="/forgot-password">Forgot password?</Link>
+            </div>
+            <button type="submit" className="signup-button">
+              Login
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
