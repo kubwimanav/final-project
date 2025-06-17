7f954e43873b1bed6907713fb6d2adceb70af881
kubwimanav <kubwimanavincent2020@gmail.com>
2025-06-17 19:19:36 +0200

    fixchannge

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     proxy: {
       // Handle /api routes
       "^/api/.*": {
-        target: "http://localhost:5000",
+        target: "http://127.0.0.1:5000",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
