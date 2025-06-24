610c37b072048fa073355d9b8a5bcd82022edbf5
Methode TWIZEYIMANA <tmmethode1@gmail.com>
2025-06-24 12:49:34 +0200

    Change backend port to 5173 and frontend port to 5000

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     host: true,
-    port: 5173,
+    port: 5000,
     allowedHosts: ['dfl.memiserve.com'],
     proxy: {
       // Handle /api routes
